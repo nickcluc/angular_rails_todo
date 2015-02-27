0ccde61adde648c77a353c800ad37a901c81cdfb
nickcluc <nclucas01@gmail.com>
2015-02-27 16:01:32 -0500

    set up rails api

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,44 @@
+module Api
+  module V1
+    class TasksController < ApplicationController
+      skip_before_filter :verify_authenticity_token
+      respond_to :json
+
+      def index
+        respond_with(Task.all.order("completed ASC").order("id DESC"))
+      end
+
+      def show
+        respond_with(Task.find(params[:id]))
+      end
+
+      def create
+        @task = Task.new(task_params)
+        if @task.save
+          respond_to do |format|
+            format.json { render json: @task }
+          end
+        end
+      end
+
+      def update
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+          respond_to do |format|
+            format.json { render json: @task }
+          end
+        end
+      end
+
+      def destroy
+        respond_with Task.destroy(params[:id])
+      end
+
+      private
+
+      def task_params
+        params.require(:task).permit(:name, :completed)
+      end
+    end
+  end
+end
